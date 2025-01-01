dcd1e07784dff315d3a4f9f77e0a4f075f745d8e
Vladimir-Pobedinskiy <VladimirPobedinskiy@yandex.ru>
2025-01-01 14:31:26 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
 	preview: {
 		port: 8080,
 	},
-	base: 'https://vladimir-pobedinskiy.github.io/portfolio-vue-vite/',
+	base: 'portfolio-vue-vite',
 	plugins: [vue(), vueDevTools(), svgLoader({ svgo: false })],
 	resolve: {
 		alias: {
