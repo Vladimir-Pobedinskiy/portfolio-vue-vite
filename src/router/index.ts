40f3dc06da683991ddc168f91ee9e81b31bf8de9
Vladimir Pobedinskiy <vladpob058@gmail.com>
2025-04-14 16:11:23 +0300

    remove 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,26 +10,6 @@ const router = createRouter({
 })
 
 router.beforeEach((to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
-	// Обработка 404 редиректа с GitHub Pages
-	if (sessionStorage.getItem('github-404-redirect')) {
-		const redirectPath = sessionStorage.getItem('github-404-redirect')
-		sessionStorage.removeItem('github-404-redirect')
-
-		// Применяем вашу логику обработки путей к редиректу
-		if (redirectPath) {
-			let path = redirectPath.replace(/\/{2,}/g, '/')
-			if (!path.endsWith('/')) {
-				path += '/'
-			}
-
-			return next({
-				path: path,
-				query: to.query,
-				hash: to.hash,
-			})
-		}
-	}
-
 	let path = to.path
 	// Удаляем лишние слэши, кроме первого
 	path = path.replace(/\/{2,}/g, '/')
