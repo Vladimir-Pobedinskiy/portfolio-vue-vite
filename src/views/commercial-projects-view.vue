b3f42cc2c03d2bd386da3f07985c8194940a886b
Vladimir-Pobedinskiy <VladimirPobedinskiy@yandex.ru>
2025-04-16 11:52:23 +0300

    new image in commercial-projects

--- src/views/commercial-projects-view.vue
+++ src/views/commercial-projects-view.vue
@@ -42,7 +42,7 @@ const portfolio = {
 	portfolioList: [
 		{
 			img: {
-				url: 'https://vladimir-pobedinskiy.github.io/portfolio-vue-vite/img/case/case-img-1.jpg',
+				url: 'https://vladimir-pobedinskiy.github.io/portfolio-vue-vite/img/case/case-img-1.webp',
 				alt: 'image',
 			},
 			type: 'Благотворительный фонд',
